91c9af51ab14c02c292ac6e4ec3d55a35cd0f6a4
Piotr Husiatyński <phusiatynski@gmail.com>
2018-08-19 09:07:40 +0200

    better id generation

--- pkg/surf/id.go
+++ pkg/surf/id.go
@@ -3,11 +3,13 @@ package surf
 import (
 	"crypto/rand"
 	"encoding/hex"
-	"time"
 )
 
 func generateID() string {
-	return <-randomID
+	if id, ok := <-randomID; ok {
+		return id
+	}
+	panic("cannot generate id")
 }
 
 var randomID chan string
@@ -18,8 +20,8 @@ func init() {
 		b := make([]byte, 16)
 		for {
 			if _, err := rand.Read(b); err != nil {
-				time.Sleep(time.Second)
-				continue
+				close(randomID)
+				panic("cannot read random data: " + err.Error())
 			}
 			randomID <- hex.EncodeToString(b)
 		}
